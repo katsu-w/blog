3997842955ba9695e6ae61773a3b491c561e64b1
katsu <sdjgsinhzoghaodg@yandex.ru>
2025-11-24 21:22:26 +0300

    Init + BLOG-003

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`
+
+function App() {
+
+  return (
+	  <>
+		  
+		  <Div>
+			  <i className="fa fa-calendar"></i>
+			  123
+			</Div>
+	  </>
+  )
+}
+
+export default App
