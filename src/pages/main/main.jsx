053c2bd75ce33b0c8995d3c1f6e204baa21f7d51
katsu <sdjgsinhzoghaodg@yandex.ru>
2026-01-09 22:00:42 +0300

    Вёрстка главной страницы

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -16,15 +16,27 @@ const MainContainer = ({ className }) => {
 	
 	return (
 		<main className={className}>
-			{posts.map(({ id, title, publishedAt, commentsCount }) => <PostCard
-				key={id}
-				id={id}
-				title={title}
-				publishedAt={publishedAt}
-				commentsCount={commentsCount}
-			/>)}
+			<div className="posts-layout">
+				{posts.map(({ id, title, imageUrl, publishedAt, commentsCount }) =>
+					<PostCard
+						key={id}
+						id={id}
+						title={title}
+						imageUrl={imageUrl}
+						publishedAt={publishedAt}
+						commentsCount={commentsCount}
+					/>)}
+			</div>
 		</main>
 	);
 };
 
-export const Main = styled(MainContainer)``;
+export const Main = styled(MainContainer)`
+	& .posts-layout {
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		gap: 50px;
+		padding: 56px;
+	}
+`;
